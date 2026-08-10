5c5fa75480ae08cb2dd38bee48740048e009f7d8
Awdhesh Mishra <awdheshmishra310@gmail.com>
2026-08-10 18:27:33 +0530

    js

---
+++ project3/css&js/script1.js
@@ -0,0 +1,86 @@
+console.log("hello world")
+
+alert("ok")
+
+let name  = prompt("enter your name")
+console.log(name)
+
+console.warn("this is warning")
+
+console.error("this is an error")
+
+let A = 10;
+let B = 20;
+let c = 40;
+// console.log("sum is: ", a+b)
+//template literals are used to add embedded expressions in a string
+let output = `the sum is: ${A+B}+${c+A} rupees`;
+console.log(output);
+
+//operators -->  Arithmatic, Unary, Assignment, Comparison, Logical
+let a = 1;
+let b = 3;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+
+//comparison
+console.log('a'>'b')
+console.log('a'<='z')
+console.log('@'=='*')
+console.log('Z'>'A')
+
+//conditional 
+age = 2
+if(age>=18){
+    console.log("eligible")
+    console.log("you can drive")
+}
+else{
+    console.log("not eligible");
+    console.log("you cannot  drive")
+}
+
+//preactice
+
+let color = "red";
+if (color==="red"){
+    console.log("stop")
+}
+else if(color === "yellow"){
+    console.log("ready")
+}
+else if(color === "green"){
+    console.log("go")
+}
+else{
+    console.log("invalid entry")
+}
+
+//question 2
+
+let size = "XL";
+// let size = "L";
+// let size = "M";
+// let size = "S";
+
+if(size === "XL"){
+    console.log("price is Rs. 250")
+}
+else if (size === "L"){
+    console.log("price is Rs. 200")
+}
+else if ( size === "M"){
+    console.log("price is Rs. 100")
+}
+else if(size === "S"){
+    console.log("price is Rs. 50")
+}
+else{
+    console.log("invalid entry")
+}
+
+
+
